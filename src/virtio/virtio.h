88bba983082e8cea164d3519d4354fee0259b0f6
Francesco Lavra <francescolavra.fl@gmail.com>
2022-05-22 12:30:27 +0200

    x86: add support for microvm Qemu machine type
    With this change, the kernel supports running on the microvm Qemu
    machine type, booting via the HVM direct boot protocol and using
    virtIO-MMIO disk and network devices.
    The memory location and interrupt number of virtIO-MMIO devices is
    retrieved via ACPI by calling the newly defined function
    virtio_mmio_enum_devs(). The code in virtio_mmio.c has been
    refactored so that it can be used to enumerate devices via either
    the kernel command line (used by Firecracker) or ACPI tables (used
    by microvm). In addition, it now supports non-page-aligned memory
    base values, and resets the devices being enumerated (which is
    needed in case a device has been initialized by code running before
    the kernel, such as the BIOS).
    The MICROVM Makefile variable can be used to run a unikernel image
    with microvm, as in `make run MICROVM=1`.
    Running an image with microvm results in faster boot times and less
    memory consumption compared to using the default q35 Qemu machine
    type.

--- src/virtio/virtio.h
+++ src/virtio/virtio.h
@@ -4,3 +4,4 @@ void init_virtio_network(kernel_heaps kh);
 void init_virtio_scsi(kernel_heaps kh, storage_attach a);
 
 void virtio_mmio_parse(kernel_heaps kh, const char *str, int len);
+void virtio_mmio_enum_devs(kernel_heaps kh);
